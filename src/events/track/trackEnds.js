ad00c22f19c9650d01b18c68d81d455f19eee2a1
ThePedroo <pedroolimpio152@gmail.com>
2023-11-23 00:09:21 -0300

    fix: wrong queues
    This commit fixes a major issue that had "replaced" as reason to go to the next track of the queue.

--- src/events/track/trackEnds.js
+++ src/events/track/trackEnds.js
@@ -10,7 +10,7 @@ function trackEnds(Event, payload, node, config, Nodes, Players) {
   if (!player)
     return console.log(`[FastLink] Received ${name} from ${node} but no player was found`)
 
-  if (name != 'trackException' && config.queue && ['replaced', 'loadFailed'].includes(payload.reason)) {
+  if (name != 'trackException' && config.queue && ['finished', 'loadFailed'].includes(payload.reason)) {
     player.queue.shift()
 
     if (player.queue.length != 0) {
