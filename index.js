029b7b32097f56a35248df648a3f8b62f3b8cf43
TehPig (TechPig) <34095153+TehPigYT@users.noreply.github.com>
2023-12-08 16:35:54 +0200

    fix: redefining const variable
    This commit fixes an issue where a const variable would have its value changed.

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ class Player {
 
     if(Players[this.guildId].queue.length < 3) throw new Error('Queue must have at least 3 tracks to be shuffled.');
 
-    for (const i = Players[this.guildId].queue.length - 1; i > 0; i--) {
+    for (let i = Players[this.guildId].queue.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = Players[this.guildId].queue[i];
       Players[this.guildId].queue[i] = Players[this.guildId].queue[j];
