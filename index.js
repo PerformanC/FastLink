4daee2f7deb20684ee1fa460bda75c0705c8c0b4
ThePedroo <pedroolimpioguerra@gmail.com>
2024-01-15 00:51:56 -0300

    update: version
    This commit updates the version based on previous commits.

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function connectNodes(nodes, config) {
         Authorization: node.password,
         'Num-Shards': config.shards,
         'User-Id': config.botId,
-        'Client-Name': 'FastLink/2.3.6'
+        'Client-Name': 'FastLink/2.4.0'
       }
     })
 
@@ -434,7 +434,7 @@ class Player {
         Authorization: Nodes[this.node].password,
         'user-id': Config.botId,
         'guild-id': this.guildId,
-        'Client-Name': 'FastLink/2.3.6'
+        'Client-Name': 'FastLink/2.4.0'
       }
     })
 
